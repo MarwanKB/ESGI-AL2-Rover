b79b43237b665c9b6cbc888ff5bb7cc35e7bd8d3
MarwanKB <marwan.boubchir@outlook.fr>
2021-09-10 12:48:13 +0200

    Build : Ajout de la position Z

--- src/main/java/fr/esgi/al2/planetrover/rover/Position.java
+++ src/main/java/fr/esgi/al2/planetrover/rover/Position.java
@@ -7,24 +7,26 @@ import java.util.Objects;
 public class Position {
     final int x;
     final int y;
+    final int z;
 
-    public Position(int x, int y) {
+    public Position(int x, int y, int z) {
         this.x = x;
         this.y = y;
+        this.z = z;
     }
 
 
     public Position goNorth() {
-            return new Position(this.x, this.y+1);
+            return new Position(this.x, this.y+1, this.z);
     }
     public Position goSouth(){
-        return new Position (this.x, this.y-1);
+        return new Position (this.x, this.y-1, this.z);
     }
     public Position goEast(){
-        return new Position (this.x+1, this.y);
+        return new Position (this.x+1, this.y, this.z);
     }
     public Position goWest(){
-        return new Position (this.x-1, this.y);
+        return new Position (this.x-1, this.y, this.z);
     }
 
     @Override
@@ -32,19 +34,11 @@ public class Position {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Position position = (Position) o;
-        return x == position.x && y == position.y;
+        return x == position.x && y == position.y && z == position.z;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(x, y);
-    }
-
-    @Override
-    public String toString() {
-        return "Position{" +
-                "x=" + x +
-                ", y=" + y +
-                '}';
+        return Objects.hash(x, y, z);
     }
 }
