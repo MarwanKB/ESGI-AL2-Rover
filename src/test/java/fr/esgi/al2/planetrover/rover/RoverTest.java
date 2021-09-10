c3bf70218c3196694d6eb8ed3406175404b653de
MarwanKB <marwan.boubchir@outlook.fr>
2021-09-10 12:46:59 +0200

    Build : Test Fonctions de toutes les fonctions (Avancer, Reculer, Tourner dans toutes les directions et depuis toutes les directions.

--- src/test/java/fr/esgi/al2/planetrover/rover/RoverTest.java
+++ src/test/java/fr/esgi/al2/planetrover/rover/RoverTest.java
@@ -5,38 +5,226 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class RoverTest {
-    Rover test =
-            new Rover(
-                    new Name("monrover"),
-                    new Position(2, 4),
-                    Orientation.North);
+
+    private Position positionAt(int x, int y){
+        return new Position (x,y,0);
+    }
+    private Position positionAt(int x, int y, int z){
+        return new Position (x,y,z);
+    }
 
     @Test
     public void testConstructeur() {
 
+        Rover test1 =
+                new Rover(
+                        new Name("monrover"),
+                        positionAt(2, 4),
+                        Orientation.North);
+
         Rover test2 =
                 new Rover(
                         new Name("monrover"),
-                        new Position(2, 5),
+                        positionAt(2, 5),
                         Orientation.North);
+
+        Assertions.assertEquals(test2,test1);
     }
 
     @Test
-    void testMove(){
-        test.move();
-        test.move();
-        Assertions.assertEquals(new Position(2,6), test.getPosition());
+    void testMoveFwNorth(){
+        Rover test1 =
+                new Rover(
+                        new Name("monrover"),
+                        positionAt(2, 4),
+                        Orientation.North);
+
+        test1.moveForward();
+        test1.moveForward();
+        Assertions.assertEquals(positionAt(2,6), test1.getPosition());
+
+    }
+    @Test
+    void testMoveFwSouth(){
+        Rover test1 =
+                new Rover(
+                        new Name("monrover"),
+                        positionAt(2, 4),
+                        Orientation.South);
+
+        test1.moveForward();
+        test1.moveForward();
+        Assertions.assertEquals(positionAt(2,2), test1.getPosition());
 
     }
 
     @Test
-    void testTurnLeft(){
-        test.turnLeft();
-        Assertions.assertEquals(Orientation.West, test.getOrientation());
+    void testMoveFwEast(){
+        Rover test1 =
+                new Rover(
+                        new Name("monrover"),
+                        positionAt(2, 4),
+                        Orientation.East);
+
+        test1.moveForward();
+        test1.moveForward();
+        Assertions.assertEquals(positionAt(4,4), test1.getPosition());
+
     }
     @Test
-    void testTurnRight() {
-        test.turnRight();
-        Assertions.assertEquals(Orientation.East, test.getOrientation());
+    void testMoveFwWest(){
+        Rover test1 =
+                new Rover(
+                        new Name("monrover"),
+                        positionAt(2, 4),
+                        Orientation.West);
+
+        test1.moveForward();
+        test1.moveForward();
+        Assertions.assertEquals(positionAt(0,4), test1.getPosition());
+
+    }
+
+    @Test
+    void testMoveBwFromNorth(){
+        Rover test1 =
+                new Rover(
+                        new Name("monrover"),
+                        positionAt(2, 4),
+                        Orientation.North);
+
+        test1.moveBackward();
+        test1.moveBackward();
+        Assertions.assertEquals(positionAt(2,2), test1.getPosition());
+
+    }
+    @Test
+    void testMoveBwFromSouth(){
+        Rover test1 =
+                new Rover(
+                        new Name("monrover"),
+                        positionAt(2, 4),
+                        Orientation.South);
+
+        test1.moveBackward();
+        test1.moveBackward();
+        Assertions.assertEquals(positionAt(2,6), test1.getPosition());
+
+    }
+    @Test
+    void testMoveBwFromEast(){
+        Rover test1 =
+                new Rover(
+                        new Name("monrover"),
+                        positionAt(2, 4),
+                        Orientation.East);
+
+        test1.moveBackward();
+        test1.moveBackward();
+        Assertions.assertEquals(positionAt(0,4), test1.getPosition());
+
+    }
+    @Test
+    void testMoveBwFromWest(){
+        Rover test1 =
+                new Rover(
+                        new Name("monrover"),
+                        positionAt(2, 4),
+                        Orientation.West);
+
+        test1.moveBackward();
+        test1.moveBackward();
+        Assertions.assertEquals(positionAt(4,4), test1.getPosition());
+
+    }
+
+    @Test
+    void testTurnLeftFromNorth(){
+        Rover test1 =
+                new Rover(
+                        new Name("monrover"),
+                        positionAt(2, 4),
+                        Orientation.North);
+
+        test1.turnLeft();
+        Assertions.assertEquals(Orientation.West, test1.getOrientation());
+    }
+    @Test
+    void testTurnLeftFromEst() {
+        Rover test1 =
+                new Rover(
+                        new Name("monrover"),
+                        positionAt(2, 4),
+                        Orientation.East);
+
+        test1.turnLeft();
+        Assertions.assertEquals(Orientation.North, test1.getOrientation());
+    }
+    @Test
+    void testTurnLeftFromWest() {
+        Rover test1 =
+                new Rover(
+                        new Name("monrover"),
+                        positionAt(2, 4),
+                        Orientation.West);
+
+        test1.turnLeft();
+        Assertions.assertEquals(Orientation.South, test1.getOrientation());
+    }
+    @Test
+    void testTurnLeftFromSouth() {
+        Rover test1 =
+                new Rover(
+                        new Name("monrover"),
+                        positionAt(2, 4),
+                        Orientation.South);
+
+        test1.turnLeft();
+        Assertions.assertEquals(Orientation.East, test1.getOrientation());
+    }
+
+    @Test
+    void testTurnRightFromNorth() {
+        Rover test1 =
+                new Rover(
+                        new Name("monrover"),
+                        positionAt(2, 4),
+                        Orientation.North);
+
+        test1.turnRight();
+        Assertions.assertEquals(Orientation.East, test1.getOrientation());
+    }
+    @Test
+    void testTurnRightFromSouth() {
+        Rover test1 =
+                new Rover(
+                        new Name("monrover"),
+                        positionAt(2, 4),
+                        Orientation.South);
+
+        test1.turnRight();
+        Assertions.assertEquals(Orientation.West, test1.getOrientation());
+    }
+    @Test
+    void testTurnRightFromEast() {
+        Rover test1 =
+                new Rover(
+                        new Name("monrover"),
+                        positionAt(2, 4),
+                        Orientation.East);
+
+        test1.turnRight();
+        Assertions.assertEquals(Orientation.South, test1.getOrientation());
+    }
+    @Test
+    void testTurnRightFromWest() {
+        Rover test1 =
+                new Rover(
+                        new Name("monrover"),
+                        positionAt(2, 4),
+                        Orientation.West);
+
+        test1.turnRight();
+        Assertions.assertEquals(Orientation.North, test1.getOrientation());
     }
 }
